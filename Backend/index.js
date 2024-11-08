c10d63538247be1213f471b4c2ca8984c4acfc14
Sandeep <sandeepgunde1@gmail.com>
2024-11-08 08:55:47 +0530

    error handling

--- Backend/index.js
+++ Backend/index.js
@@ -7,37 +7,43 @@ const {prismaClient, PrismaClient} = require('@prisma/client')
 app.use(bodyparser.json())
 
 const corsOption = {
-     origin: 'https://fnxdlfrontend.vercel.app', // Allow only this domain
-     methods: ['GET', 'POST'], // Allow specific methods (optional)
+     origin: 'https://fnxdlfrontend.vercel.app',
+     methods: ['GET', 'POST'],
      allowedHeaders: ['Content-Type', 'Authorization'], 
     }
-
 app.use(cors(corsOption))
 
 async function createUser(userData)
 {
-    const prisma  = new PrismaClient()
-    const userCreated = await prisma.Users.create({ 
-        data:{
-            "first_name":userData.firstName,
-            "email":userData.email,
-            "last_name":userData.lastName,
-            "phone_number":userData.phone,
-            "graduation_year":userData.graduationYear,
-            "city":userData.city
-        }}
-    )
-    return userCreated
+    if(userData.firstName==="" ||userData.email===""||userData.lastName===""||userData.phone===""||userData.graduationYear===""||userData.city==="")
+    {
+        return 0
+    }
+    else{
+            const prisma  = new PrismaClient()
+            const userCreated = await prisma.Users.create({ 
+                data:{
+                    "first_name":userData.firstName,
+                    "email":userData.email,
+                    "last_name":userData.lastName,
+                    "phone_number":userData.phone,
+                    "graduation_year":userData.graduationYear,
+                    "city":userData.city
+                }}
+            )
+            return userCreated
+        }
 }
 app.post('/downloadBroucher',(req,res)=>{
     const createdUser = createUser(req.body)
-    if(createUser)
+    if(createdUser)
     {
         res.json({message:"User Created"})
     }
     else{
-        res.status(401).json({message:"something went wrong"})
+        res.status(401).json({message:"Field is empty"})
     }
+    res.status(404).json({message:"something went wrong"})
 })
 
 app.listen(4000,()=>{
